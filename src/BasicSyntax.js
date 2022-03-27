6f946a84d9d2c34095cdbb8ad4d4678a2d6681e1
Валентина Кобцева <kvaaa0601@gmail.com>
2022-03-27 19:32:05 +0700

    Задание практика

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,13 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    let romanSymb = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    for (let i = 0; i < str.length; i++) {
+        if (romanSymb[str[i]] < romanSymb[str[i + 1]]) {
+            result -= romanSymb[str[i]];
+        } else {
+            result += romanSymb[str[i]];
+        }
+    }
     return result;
 }
